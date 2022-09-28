3f55d7a0eed018385b677fefbc37c5cd51eabb65
Pinapple23 <92719911+Pinapple23@users.noreply.github.com>
2022-09-28 20:56:31 +0200

    input trial1
    Input is not working

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,12 +4,15 @@ import uuid from 'react-uuid';
 //NOTE Interface is a structure which acts as a contract in our application.
 //It defines the syntax for classes to follow, means a class which implements
 //an interface is bound to implement all its members.
+
+const key = uuid();
 interface InputProps {
     type: string;
     className: string;
     value: string;
     name: string;
     placeholder: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 //NOTE :FC means that our component is a FunctionComponent
 
@@ -19,13 +22,13 @@ const InputField: FC<InputProps> = ({
     className,
     name,
     value,
-    placeholder
+    placeholder,
+    onChange
 }) => {
     //NOTE: The main purpose of keys is to help React
     // differentiate and distinguish elements from each other, increasing its
     // performance when diffing between the virtual and real DOM.
-    const key = uuid();
-    console.log(key);
+
     return (
         <>
             <input
@@ -36,6 +39,7 @@ const InputField: FC<InputProps> = ({
                 placeholder={placeholder}
                 key={key}
                 required
+                onChange={onChange}
             />
         </>
     );
--- src/features/SignUp/SignUp.tsx
+++ src/features/SignUp/SignUp.tsx
@@ -1,8 +1,23 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import InputField from '../../components/InputField';
 
 const SignUp: FC = () => {
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    const changeHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        setForm({
+            ...form,
+            [e.currentTarget.name]: e.currentTarget.value
+        });
+    };
+
+    const showState = () => {
+        console.log(form);
+    };
     return (
         <>
             <div className="h-screen w-1/2 pos fixed z-[1] top-0 overflow-hidden left-0 bg-rdx-purple"></div>
@@ -12,10 +27,11 @@ const SignUp: FC = () => {
                     src="src/assets/Images/RadicalX logo.png"
                     className="top-[77px] left-[1034px] w-[200px] h-[40px] fixed"
                 />
-                <form className="top-[363px] left-[954px] fixed flex-auto space-y-8">
+                <div className="top-[363px] left-[954px] fixed flex-auto space-y-8">
                     <p className="text-xl font-medium tracking-[.4px] leading-[24px]">
                         Sign Up
                     </p>
+
                     <InputField
                         type="text"
                         //NOTE just example of using tailwind css
@@ -23,6 +39,7 @@ const SignUp: FC = () => {
                         name="email"
                         value=""
                         placeholder="Email"
+                        onChange={changeHandler.toString}
                     />
                     <InputField
                         type="text"
@@ -31,15 +48,17 @@ const SignUp: FC = () => {
                         name="password"
                         value=""
                         placeholder="Password"
+                        onChange={changeHandler.toString}
                     />
                     <button
                         name="signUp"
                         className="w-[360px] h-[48px] rounded-2xl  bg-rdx-purple text-[#FFFFFF] border-2 border-[#CECECE] "
+                        onClick={showState}
                     >
                         {' '}
                         Sign up{' '}
                     </button>
-                </form>
+                </div>
             </div>
         </>
     );
