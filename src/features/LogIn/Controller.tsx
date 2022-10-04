e45b2bab00296916659b4780fd45c41a4929d3ab
RadwaTaha1999 <110698134+RadwaTaha1999@users.noreply.github.com>
2022-10-04 05:56:34 +0200

    Login page complete without routing

--- src/features/LogIn/Controller.tsx
+++ src/features/LogIn/Controller.tsx
@@ -1,5 +1,14 @@
 //TODO:here we add functions that validate inputs in the client side
 //set error messages for each input
 
-export const validateEmail = (email: string) => {};
-export const validatePassword = (password: string) => {};
+export const validateEmail = (email: string,  setEmailErrorMessage: React.Dispatch<React.SetStateAction<string>>) => {
+    setEmailErrorMessage('');
+};
+export const validatePassword = (
+    password: string,
+    setPasswordErrorMessage: React.Dispatch<React.SetStateAction<string>>
+) => {
+    setPasswordErrorMessage('');
+
+  
+};
