72d3791dfe98fa638f63f387697ccde0841e086e
Hazem Abdo <ha4450770@gmail.com>
2022-10-04 18:00:21 +0200

    adding a list of tags

--- src/features/Card/Box/Box.tsx
+++ src/features/Card/Box/Box.tsx
@@ -6,7 +6,7 @@ import Ico3 from '../logs.tsx/ico3';
 import Tag from '../Tag/Tag';
 const Box = () => {
     const Tags = ['Manager', 'Manager', 'Manager', 'Manager'];
-    const Orders=["order-none" ,"order-1" ,"order-none","order-1"]
+    const Orders = ['order-none', 'order-1', 'order-none', 'order-1'];
     return (
         <div className="box-border flex flex-col items-start p-5 h-56 bg-white flex-none order-none self-stretch flex-grow-0 w-[365px] border-[#CFD3D9] border rounded-lg ">
             <div className="inline-flex">
@@ -15,10 +15,10 @@ const Box = () => {
             <div className="mb-[35px]">
                 <Paragraph text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magaliqua." />
             </div>
-            <div className="flex flex-col items-start p-0 h-16 flex-none order-1 flex-grow-0 w-[283px] gap-[8xp] "> 
-                {Tags.map(tag => (
-                    <Tag text={tag}  order={Orders[index]}/>
-                ))}
+            <div className="flex flex-col items-start p-0 h-16 flex-none order-1 flex-grow-0 w-[283px] gap-[8xp] ">
+                {Tags.map((tag, index) => {
+                    return <Tag text={tag} order={Orders[index]} key={index} />;
+                })}
             </div>
         </div>
     );
--- src/features/Card/Tag/Tag.tsx
+++ src/features/Card/Tag/Tag.tsx
@@ -1,11 +1,14 @@
 import { FC, useState } from 'react';
 interface TagProps {
-    text:string;
+    text: string;
+    order: string;
 }
-const Tag: FC<TagProps>  = ({text}) => {
+const Tag: FC<TagProps> = ({ text, order }) => {
     return (
-        <div className='box-border flex flex-row items-start w-32 h-6 bg-indigo-100 rounded-lg flex-none order-none flex-grow-0 border-[#793EF5] p-[3px] gap-[10px] justify-center'>
-            <div className='w-24 h-4 not-italic font-medium text-xs leading-4 flex items-center text-purple-600 flex-none order-none flex-grow-0 justify-center '>
+        <div
+            className={`box-border flex flex-row items-start w-32 h-6 bg-indigo-100 rounded-lg flex-none order-none flex-grow-0 border-[#793EF5] p-[3px] gap-[10px] justify-center ${order}`}
+        >
+            <div className="w-24 h-4 not-italic font-medium text-xs leading-4 flex items-center text-purple-600 flex-none order-none flex-grow-0 justify-center ">
                 {text}
             </div>
         </div>
